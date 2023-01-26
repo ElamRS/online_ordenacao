a9666d6ca0c4a2eb8a17068c830e9d4ae939846e
Elam Rufino <107227033+ElamRS@users.noreply.github.com>
2023-01-26 08:49:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 function ordenar() {
     let lista = document.getElementById('lista').value;
-    alert(Math.floor(lista.length/2))
+
+    let ordenado = document.getElementById('ordenado');
+
+    // Define estilos universais
+    ordenado.style.fontSize = "20px";
+    ordenado.style.marginTop = "1vw";
+
+    const ord = [];
+
+    if (checarInput(lista)) {
+        const ord = [];
+        for (i=0, j=0; i<lista.length; ++i) {
+            if (!(lista[i]=="," || lista[i]==" ")) {
+                ord[j] = lista[i];
+                j++;
+            }
+        }
+        ord.sort(function(a, b){return a - b});
+        ordenado.style.color = "blue";
+        ordenado.innerHTML = ord;
+    } else {
+        ordenado.style.color = "red";
+        ordenado.innerHTML = `Formato não suportado.`;
+    }
 }
 
 function checarInput(l) {
-    let espaco, virgula=0;
+    let espaco=0;
+    let virgula=0;
     // Se a lista for passada da forma como foi prevista, ela terá um tamanho ímpar
     if ((l.length%2)==1) {
         for (i=0; i<l.length; ++i) {
-            if (l[i]==' ') {espaco++;}
-            else if(l[i]==',') {virgula++;}
+            if (l[i]==' ') {
+                espaco++;
+            } else if(l[i]==',') {
+                virgula++;
+            }
         }
     } else {
         return false;
@@ -18,8 +45,4 @@ function checarInput(l) {
     if (espaco==(Math.floor(l.length/2)) || virgula==(Math.floor(l.length/2))) {
         return true;
     }
-}
-
-function quickSort() {
-
 }
